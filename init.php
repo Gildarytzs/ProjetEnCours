9368bdfea95d7e55f79ea557150e5def70d03c26
root <root@Machattrapage.com>
2016-08-14 20:33:13 +0200

    Eurreur 20h32

--- init.php
+++ init.php
@@ -2,6 +2,7 @@
 session_start();
 require "php/conf.inc.php";
 require "functions.php";
+include "php/access.php";
 
 
 function connectBdd() {
--- php/access.php
+++ php/access.php
@@ -23,3 +23,18 @@ function logout() {
     session_destroy();
     header('Location: index.php');
 }
+function isConnected() {
+    if (!isset($_SESSION['access_token']) || empty($_SESSION['access_token'])) {
+        return FALSE;
+    }
+    $bdd = connectBdd();
+    $query = $bdd->prepare('SELECT id FROM users WHERE email = :email AND access_token = :access_token');
+    $query->execute(["email" => $_SESSION['email'], "access_token" => $_SESSION['access_token']]);
+    $resultat = $query->fetch();
+    if (empty($resultat)) {
+        return FALSE;
+    } else {
+        $_SESSION['access_token'] = generateAccessToken($_SESSION['email']);
+        return TRUE;
+    }
+}
